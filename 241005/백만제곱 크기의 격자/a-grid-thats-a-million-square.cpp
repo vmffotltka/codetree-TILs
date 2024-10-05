4306877a7c24a53e0cf393cfd09fe6ffef78c428
유현서 <vmffotltka1@gmail.com>
2024-10-05 19:39:01 +0900

    Title: 백만제곱 크기의 격자, Time: 262ms, Memory: 20MB, Status: Passed - CodeTree

--- 241005/백만제곱 크기의 격자/a-grid-thats-a-million-square.cpp
+++ 241005/백만제곱 크기의 격자/a-grid-thats-a-million-square.cpp
@@ -1,46 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int N;
-int dy[4] = { 0, 0, 1, -1 };
-int dx[4] = { 1, -1, 0, 0 };
+int dy[8] = { 1, 1, 1, 0, -1, -1, -1, 0 };
+int dx[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
 map<int, map<int, int>> arr;
+
+int countCover(int y, int x) {
+    int cnt = 0;
+    for (int i = 1; i < 8; i += 2) {
+        int ny = y + dy[i], nx = x + dx[i];
+        if (arr[ny][nx] == 1) cnt++;
+    }
+    return cnt;
+}
 
 int bfs(int y, int x) {
     deque<pair<int, int>> dq;
     dq.push_back({ y, x });
     arr[y][x] = 2;
-    int mxY = y, mnY = y, mxX = x, mnX = x;
+    int ret = 0;
     while (!dq.empty()) {
         auto [cy, cx] = dq.front(); dq.pop_front();
-        for (int i = 0; i < 4; i++) {
+        int cnt = countCover(cy, cx);
+        if (!cnt) continue;
+        ret += cnt;
+        for (int i = 0; i < 8; i++) {
             int ny = cy + dy[i], nx = cx + dx[i];
-            if (arr[ny][nx] == 1) {
-                dq.push_back({ ny, nx });
+            if (arr[ny][nx] == 0) {
+                if (!(i & 1)) {
+                    int nextY1 = cy + dy[(i + 1) % 8], nextX1 = cx + dx[(i + 1) % 8];
+                    int nextY2 = cy + dy[(i + 7) % 8], nextX2 = cx + dx[(i + 7) % 8];
+                    if (arr[nextY1][nextX1] == 1 && arr[nextY2][nextX2] == 1)
+                        continue;
+                }
                 arr[ny][nx] = 2;
-                mxY = max(mxY, ny), mnY = min(mnY, ny);
-                mxX = max(mxX, nx), mnX = min(mnX, nx);
+                dq.push_back({ ny, nx });
             }
         }
     }
-    return (mxX - mnX + mxY - mnY + 2) * 2;
+    return ret;
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> N;
-    int sy = -1, sx = -1;
+    int mxY = -1, mxX = -1;
     for (int i = 0; i < N; i++) {
         int y, x; cin >> y >> x;
         arr[y][x] = 1;
-        if (sy == -1) sy = y, sx = x;
+        if (mxY < y) mxY = y, mxX = x;
+        else if (mxY == y) mxX = max(mxX, x);
     }
-    int ans = 0;
-    for (auto &[y, indexes] : arr) {
-        for (auto &[x, val] : indexes) {
-            if (val == 1) ans += bfs(y, x);
-        }
-    }
-    cout << ans;
+    cout << bfs(mxY, mxX + 1);
 }
