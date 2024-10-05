dba87583f300e0c3163bbec171dd3eb8f34c8a6c
유현서 <vmffotltka1@gmail.com>
2024-10-05 15:05:20 +0900

    Title: 카드 묶어서 버리기, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 241005/카드 묶어서 버리기/tie-the-card-and-throw-it-away.cpp
+++ 241005/카드 묶어서 버리기/tie-the-card-and-throw-it-away.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-    int n, ans = 0; cin >> n;
-    stack<int> st;
+    ll n, ans = 0; cin >> n;
+    stack<ll> st;
     for (int i = 0; i < n; i++) {
-        int p; cin >> p;
+        ll p; cin >> p;
         if (!st.empty() && st.top() > p) {
             ans += st.top() - p;
             while (!st.empty() && st.top() > p) st.pop();
