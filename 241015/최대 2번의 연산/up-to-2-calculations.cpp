7046ed0a7fac2f817b6578c821270bdb82a7de18
유현서 <vmffotltka1@gmail.com>
2024-10-15 14:16:29 +0900

    Title: 최대 2번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241015/최대 2번의 연산/up-to-2-calculations.cpp
+++ 241015/최대 2번의 연산/up-to-2-calculations.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     int a; cin >> a;
-    cout << (a & 1 ? (a + 1) / 2 : a / 2);
+    if (a % 2 == 0) a /= 2;
+    if (a & 1) a = (a + 1) / 2;
+    cout << a;
 }
