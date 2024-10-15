7ea4361bb3c0710e190a2e4775013e383200055f
유현서 <vmffotltka1@gmail.com>
2024-10-15 14:13:47 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241015/살 수 있는 물건 2/things-able-to-buy-2.cpp
+++ 241015/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -6,5 +6,6 @@ int main() {
     int n; cin >> n;
     if (n >= 3000) cout << "book";
     else if (n >= 1000) cout << "mask";
-    else cout << "pen";
+    else if (n >= 500) cout << "pen";
+    else cout << "no";
 }
