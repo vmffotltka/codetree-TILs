763570765951fe95d08539a20275cba50555ed46
유현서 <vmffotltka1@gmail.com>
2024-10-16 15:36:54 +0900

    Title: 두 번의 연산, Time: 289ms, Memory: 14MB, Status: Passed - CodeTree

--- 241016/두 번의 연산/two-operations.js
+++ 241016/두 번의 연산/two-operations.js
@@ -5,9 +5,9 @@ process.stdin.on('data', function (data) {
 
     if (a % 2 == 1) {
         a = a + 3;
-        if (a % 3 == 0) {
-            a = a / 3;
-        }
+    }
+    if (a % 3 == 0) {
+        a = a / 3;
     }
     console.log(a);
 });
