d529509018ec8b09b60787d67099dc764a92a078
유현서 <vmffotltka1@gmail.com>
2024-10-18 12:34:49 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e; cin >> a >> b >> c >> d >> e;
+    cout << (a > b) << '\n';
+    cout << (a > c) << '\n';
+    cout << (a > d) << '\n';
+    cout << (a > e) << '\n';
+}
