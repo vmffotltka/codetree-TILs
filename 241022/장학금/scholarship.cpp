8f912e5fef47ca0e90b1a9b5c37948979860caba
유현서 <vmffotltka1@gmail.com>
2024-10-22 13:01:06 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/장학금/scholarship.cpp
+++ 241022/장학금/scholarship.cpp
@@ -5,8 +5,8 @@ int main() {
 	cin.tie(0)->sync_with_stdio(0);
     	int mid, final; cin >> mid >> final;
         	if (mid >= 90) {
-            		if (final >= 95) cout << 10;
-                    		else if (final >= 90) cout << 5;
+            		if (final >= 95) cout << 100000;
+                    		else if (final >= 90) cout << 50000;
                             		else cout << 0;
                                     	}
                                         	else {
